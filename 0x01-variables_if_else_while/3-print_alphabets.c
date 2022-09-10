7cb530f6e0277ac9232d5dfea38bcbe73f77aafd
“Stephen <smartstevek@gmail.com>
2022-09-09 17:51:57 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,15 @@
  */
 int main(void)
 {
-		char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-			int i;
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-				for (i = 0; i < 26; i++)
-						{
-									putchar(alp[i]);
-										}
-					putchar('\n');
-						return (0);
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+
+	putchar('\n');
+	return (0);
+
 }
