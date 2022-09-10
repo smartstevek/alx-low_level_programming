94bf379aa123e8bebecc885dc8f9913b8ba79dbd
“Stephen <smartstevek@gmail.com>
2022-09-09 17:57:39 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,17 @@
  */
 int main(void)
 {
-		int i;
+	int i;
 
-			for (i = 97; i < 123; i++)
-					{
-								if (i != 101 && i != 113)
-											{
-															putchar(i);
-																	}
-									}
-				putchar('\n');
-					return (0);
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+
 }
