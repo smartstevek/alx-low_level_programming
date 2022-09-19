b988ea0385027fa2acb6be4ccd6e2a1b17591724
“Stephen <smartstevek@gmail.com>
2022-09-19 06:25:52 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,10 @@
-fndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
+wap_int(int *a, int *b);
 
-#endif /* MAIN_H */
+endif /* MAIN_H */
