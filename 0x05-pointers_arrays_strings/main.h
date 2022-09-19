a8f9463f86cf75381a75986bbc457d079bfa4843
“Stephen <smartstevek@gmail.com>
2022-09-19 12:26:18 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _main_H_
+#define _main_H_
 
-#include <stdio.h>
-
-int _putchar(char c);
 void reset_to_98(int *n);
-wap_int(int *a, int *b);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
-endif /* MAIN_H */
+#endif
