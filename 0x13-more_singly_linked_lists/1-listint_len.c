668eed50eb51ab7a492fa4e2b7155e1c9a7339c8
“Stephen <smartstevek@gmail.com>
2022-10-27 02:39:10 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
-* listint_len - returns the number of elements in
-* a lincked list
-@h: head of a list.
-*
-Return: number of nodes.
-*/
-size_t listint_len(const lintint_t *h)
+ * listint_len - returns the number of elements in
+ * a linked list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
+size_t listint_len(const listint_t *h)
 {
 	size_t nnodes = 0;
 
